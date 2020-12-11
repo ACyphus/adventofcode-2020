5a29f44ee84ece16799c0cade67bb5edacde4b38
acyphus <983880+ACyphus@users.noreply.github.com>
2020-12-11 14:39:02 -0600

    Add proper returns to d1* code

--- d1c1.rb
+++ d1c1.rb
@@ -7,6 +7,7 @@ input.each do |first_number|
     if first_number + second_number == 2020
       result = first_number * second_number
       puts "Success! #{first_number} * #{second_number} = #{result}"
+      return
     end
   end
 end
--- d1c2.rb
+++ d1c2.rb
@@ -8,6 +8,7 @@ input.each do |first_number|
       if first_number + second_number + third_number == 2020
         result = first_number * second_number * third_number
         puts "Success! #{first_number} * #{second_number} * #{third_number} = #{result}"
+        return
       end
     end
   end
